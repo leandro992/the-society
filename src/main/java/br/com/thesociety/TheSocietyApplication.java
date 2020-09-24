a54ba062c10ab685dc58a2657ce6dc701d6eb033
Victor Leandro <victorleandro100@outlook.com>
2020-09-23 21:56:49 -0300

    fist commit

---
+++ src/main/java/br/com/thesociety/TheSocietyApplication.java
@@ -0,0 +1,13 @@
+package br.com.thesociety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheSocietyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheSocietyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/thesociety/TheSocietyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thesociety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheSocietyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
