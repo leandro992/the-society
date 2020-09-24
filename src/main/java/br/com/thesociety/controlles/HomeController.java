c67a6cf62043f824ac40426e884a252beaaa3e61
Victor Leandro <victorleandro100@outlook.com>
2020-09-24 09:07:15 -0300

    Criado tela de home

--- src/main/java/br/com/thesociety/controlles/HomeController.java
+++ src/main/java/br/com/thesociety/controlles/HomeController.java
@@ -1,13 +1,14 @@
 package br.com.thesociety.controlles;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
-	public String index() {
+	@GetMapping("/home")
+	public String home() {
 		System.out.println("Entrando na home no casa de código");
 		return "home";
 	}
